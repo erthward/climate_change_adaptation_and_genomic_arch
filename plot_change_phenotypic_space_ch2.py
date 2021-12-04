0bafd224c9963cd3dae65b4b232809de26099057
Drew Terasaki Hart <drew.terasaki.hart@gmail.com>
2021-12-03 19:05:45 -0500

    Fine-tuned the phenotype-shift plotting code a bit. Fun, I learned more about controlling matplotlib saved figures. Also now ready to run in a loop over all scenarios on Savio.

--- plot_change_phenotypic_space_ch2.py
+++ plot_change_phenotypic_space_ch2.py
@@ -20,6 +20,16 @@ title_fontsize = 24
 axislab_fontsize = 16
 ticklab_fontsize = 11
 annot_fontsize = 8
+cbar_fontsize = 11
+fig_width = 14
+fig_height = 5
+dpi = 400
+
+# way to hard-code the vmax values for the scenarios, given that it would
+# be kind of a pain to write code to do just this
+linkages = ['independent', 'weak', 'strong']
+genicities =  [2, 4, 10, 20, 50, 100]
+vmaxes = {l: {g: 0.3 for g in genicities} for l in linkages}
 
 
 def get_min_pw_diff(vals):
@@ -63,7 +73,10 @@ def plot_phenotypic_shift(linkage, genicity):
 
 
     # create the figure
-    fig = plt.figure()
+    fig = plt.figure(dpi=dpi,
+                     figsize=(fig_width, fig_height)
+                    )
+    gs = fig.add_gridspec(nrows=1, ncols=3, width_ratios=[0.95, 0.95, 1.15])
 
     # loop through the three time steps to be analyzed
     time_steps = {'before': 2499,
@@ -72,7 +85,7 @@ def plot_phenotypic_shift(linkage, genicity):
     for time_step_n, time_step_info in enumerate(time_steps.items()):
         title, time_step = time_step_info
 
-        ax = fig.add_subplot(1, 3, time_step_n+1)
+        ax = fig.add_subplot(gs[0, time_step_n])
         #ax.axis('off')
         ax.set_title(title, fontdict={'fontsize': title_fontsize})
 
@@ -115,14 +128,15 @@ def plot_phenotypic_shift(linkage, genicity):
         # plot the heatmap
         sns.heatmap(arr,
                     #vmin=0,
-                    #vmax=0.6, # TODO: BETTER WAY TO SET THIS???
-                    annot=annot,
-                    annot_kws = {'fontsize': annot_fontsize},
+                    vmax=vmaxes[linkage][genicity],
+                    #annot=annot,
+                    #annot_kws = {'fontsize': annot_fontsize},
                     fmt="",
                     cmap=Acton_20_r.mpl_colormap,
                     #cmap=PinkYl_7.mpl_colormap,
                     #cmap=mpl.cm.Wistia,
-                    cbar=False,
+                    #cbar=False,
+                    cbar=time_step_n==2,
                     linewidths=0.3,
                     ax=ax)
 
@@ -148,7 +162,34 @@ def plot_phenotypic_shift(linkage, genicity):
         ax.invert_yaxis()
         ax.tick_params(axis=u'both', which=u'both',length=0) # make ticks len==0
 
-    plt.show()
+    # adjust colorbar label fontsize
+    fig.get_axes()[-1].tick_params(labelsize=cbar_fontsize)
+
+    # adjust suplot spacing
+    plt.subplots_adjust(left=0.05,
+                        bottom=0.11,
+                        right=0.99,
+                        top=0.90,
+                        wspace=0.05,
+                        hspace=None)
+
+    # return fig
+    return fig
 
 
-plot_phenotypic_shift('independent', 2)
+# produce plots for all scenarios
+for linkage in ['independent', 'weak', 'strong']:
+    for genicity in [2, 4, 10, 20, 50, 100]:
+        dirname_patt = 'mod-non-null_L%s_G%i_its0_' % (linkage, genicity)
+        dirs = os.listdir('.')
+        candidate_dirs = [d for d in dirs if re.search(dirname_patt, d)]
+        if len(candidate_dirs) > 0:
+            # make the fig
+            fig = plot_phenotypic_shift(linkage, genicity)
+            # save the fig
+            fig.savefig('phenotypic_shift_L%s_G%i.png' % (linkage, genicity),
+                        dpi=dpi,
+                        orientation='landscape',
+                       )
+
+plt.show()
