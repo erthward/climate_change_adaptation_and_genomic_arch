Changed the env change event to have 250 increments (instead of 50), and fixed the null scenario (so that it has selection and the gradient, but no environmental change.

sim/batch_script.py
@@ -121,7 +121,7 @@ else:
     params_filepath=(('/global/scratch/users/drewhart/ch2/'
                       'climate_change_adaptation_and_genomic_arch/sim/template_params.py'))
     # path to dir for output CSVs
-    output_path = '/global/scratch/users/drewhart/ch2/output/output'
+    output_path = '/global/scratch/users/drewhart/ch2/output/output_correct_null'
 
 # make sure job is running in the right directory
 #os.chdir(output_path)
@@ -435,18 +435,19 @@ def set_params(params, linkage, genicity, nullness):
         trt['alpha_distr_mu'] = 1/genicity
 
     # if this is a null sim, edit the params so that
-    # no env change occurs and there is no gradient
+    # no env change occurs 
     if nullness == 'null':
         copy_params['landscape']['layers']['shift'].pop('change');
-        copy_params['landscape']['layers']['shift']['init'][
-            'defined']['rast'] = np.ones((50, 50))*0.5
-        copy_params['landscape']['layers']['stable']['init'][
-            'defined']['rast'] = np.ones((50, 50))*0.5
+        # DETH: 2022-01-05: change the null to still have gradient but no shift
+        #copy_params['landscape']['layers']['shift']['init'][
+        #    'defined']['rast'] = np.ones((50, 50))*0.5
+        #lcopy_params['landscape']['layers']['stable']['init'][
+        #    'defined']['rast'] = np.ones((50, 50))*0.5
         # DETH: 2022-01-04: and, experimentally, get rid of selection
         # (without getting rid of fitness values)
-        for trt_i in range(2):
-            copy_params['comm']['species']['spp_0'][
-                'gen_arch']['traits']['trait_%i' % trt-i]['phi'] = 0
+        #for trt_i in range(2):
+        #    copy_params['comm']['species']['spp_0'][
+        #        'gen_arch']['traits']['trait_%i' % trt-i]['phi'] = 0
 
 
     return copy_params, genarch_filename

sim/template_params.py
@@ -9,7 +9,7 @@ stable = np.vstack([np.linspace(1, 0, 50) for _ in range(50)])
 K = np.ones((50,50))
 
 # show the landscape, for debugging, if requested
-debug_landscape = False
+debug_landscape = True
 if debug_landscape:
     import matplotlib.pyplot as plt
     fig = plt.figure()
@@ -120,7 +120,7 @@ params = {
                         #ending timestep of event
                         'end_t':            2749,
                         #number of stepwise changes in event
-                        'n_steps':          50,
+                        'n_steps':          250,
                         }, # <END> event 0
 
                     }, # <END> 'change'
@@ -356,7 +356,6 @@ params = {
                     'n_recomb_paths_mem':       int(1e4),
                     #total number of recomb paths to simulate
                     'n_recomb_paths_tot':       int(1e5),
-                    #
                     'n_recomb_sims':            100_000,
                     #whether to generate recombination paths at each timestep
                     'allow_ad_hoc_recomb':       False,