35b7904af1154e61a96e54cac36caeaf9cdfa257
Drew Terasaki Hart <drew.terasaki.hart@gmail.com>
2022-01-29 16:35:57 -0500

    Made more fixes to dataset-counting script.

--- sim/count_complete_datasets.py
+++ sim/count_complete_datasets.py
@@ -53,7 +53,11 @@ for pid in all_pids_in_dir:
                 curr_dir_patt = dir_patt % (nullness, linkage, genicity, pid)
                 dirs = [f for f in os.listdir(datadir) if re.search(curr_dir_patt,
                                                                     f)]
-                assert len(dirs)==1, ('%s' % str(dirs))
+                if run_asserts:
+                    assert len(dirs)==1, ('Did not find exactly 1 dir for '
+                                          '%s, %s linkage, % genes/trait.\n\n'
+                                          'Instead, found:\n%s\n\n') %
+                                          (nullness, linkage, genicity, str(dirs))
 
                 # check that the dir has a complete dataset
                 for d in dirs:
