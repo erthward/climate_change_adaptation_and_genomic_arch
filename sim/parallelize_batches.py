841482000a705e3c0494b54d9e14c824f542dcd3
Drew Terasaki Hart <drew.terasaki.hart@gmail.com>
2022-03-01 00:00:49 -0500

    Made tweaks to the scripts to try to run a full set of simulations dumping data every other timestep during climate change, to plot allele frequency shifts during it.

--- sim/parallelize_batches.py
+++ sim/parallelize_batches.py
@@ -12,9 +12,9 @@ if __name__ == '__main__':
     mp.set_start_method('spawn')
 
     # make the pool
-    pool = mp.Pool(int(ncpu/2))
-    #pool = mp.Pool(ncpu)
-    #pool = mp.Pool(1)
+    # DETH: SWITCH BACK AFTER SUCCESSFULLY RUNNING WITH OUTPUT EVERY OTHER TS
+    #pool = mp.Pool(int(ncpu/2))
+    pool = mp.Pool(1)
 
     # map a serial list of batch numbers into the pool
     pool.map_async(run_batch_script, [*range(ncpu)])
