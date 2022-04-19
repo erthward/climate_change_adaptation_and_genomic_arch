225ee517ff885e1e320d37797d3970b026502cab
Drew Terasaki Hart <drew.terasaki.hart@gmail.com>
2022-04-19 00:04:39 -0400

    Started working on a script to make final, paneled images, but then decided it's just not worth the effort. Will just manually make them.

--- analysis/make_final_figures.py
+++ analysis/make_final_figures.py
@@ -3,37 +3,6 @@ import numpy as np
 from PIL import Image
 import os, re, sys
 
-# loosen PIL Image-size limit, to prevent DecompressionBombWarning error
-Image.MAX_IMAGE_PIXELS = None
-
-# make paired time series and boxplot plots
-
-
-
-
-
-
-
-
-
-
-ims = {f:Image.open(os.path.join(analysis_dir, f), 'r') for f in im_files}
-
-# get size of an image
-width, height = [*ims.values()][0].size
-
-# set sizes of margins to be cropped and replaced with white
-crop_left = 0
-crop_top = 0
-crop_right = 0
-crop_bott = 0
-
-
-# generate the paneled output image
-nrow=1
-ncol=2
-out_im = Image.new('RGB',(int(width*ncol)-2*crop_left+2*crop_right,
-                          int(height*nrow)-2*crop_top+2*crop_bott))
 
 # function to pad an image
 # adapted from: https://note.nkmk.me/en/python-pillow-add-margin-expand-canvas/
@@ -46,57 +15,41 @@ def add_margin(pil_img, left, top, right, bottom, color='white'):
     return result
 
 
-# fill the output image with the input images
-for i, linkage in enumerate(['independent', 'weak', 'strong']):
-    for j, genicity in enumerate(genicities):
+# loosen PIL Image-size limit, to prevent DecompressionBombWarning error
+Image.MAX_IMAGE_PIXELS = None
 
-        # get the current image
-        if plot_type == 'DENS':
-            curr_im_filename_patt = '^pop_density_shift_L%s_G\d*%i_%sREDUND\.png' % (
-                                                                    linkage,
-                                                                    genicity,
-                                                                    redundancy)
-        else:
-            curr_im_filename_patt = '^phenotypic_shift_L%s_G\d*%i%s_%sREDUND\.png' % (
-                        linkage,
-                        genicity,
-                        '_NULL' * (nullness == 'null'),
-                        redundancy)
-        curr_im = [v for k, v in ims.items() if re.search(curr_im_filename_patt,
-                                                          k)]
-        assert len(curr_im) == 1
-        curr_im = curr_im[0]
+# make paired Nt and mean fit time series and boxplot figures
+for stat in ['mean_fit', 'Nt']:
+    ts_files = ['ch2_%s_loREDUND_over_time.jpg' % stat,
+                'ch2_%s_hiREDUND_over_time.jpg' % stat,
+               ]
+    box_files = ['boxplot_delta_%s_loREDUND.jpg' % re.sub('mean_', '', stat),
+                 'boxplot_delta_%s_hiREDUND.jpg' % re.sub('mean_', '', stat),
+                ]
+    ts_ims = {f:Image.open(f, 'r') for f in ts_files}
+    box_ims = {f:Image.open(f, 'r') for f in box_files}
 
-        # crop the current image's margins and replace them with white,
-        # to avoid repeating mariginal info in output image
-        crop_box = [0,0,curr_im.size[0], curr_im.size[1]]
-        margins_to_add = [0,0,0,0]
-        # crop the lefts, if not in the left row
-        if j > 0:
-            crop_box[0] = crop_left
-            margins_to_add[0] = crop_left
-        # crop the image tops, if not in the top row
-        #curr_im = curr_im.crop((0, 100, 0, 0))
-        if i > 0:
-            crop_box[1] = crop_top
-            margins_to_add[1] = crop_left
-        # crop the rights, if not in the right row
-        if j < 2:
-            crop_box[2] = crop_box[2] + crop_right
-            margins_to_add[2] = -crop_right
-        # crop the bottoms, if not in the bottom rows
-        if i < 2:
-            crop_box[3] = crop_box[3] + crop_bott
-            margins_to_add[3] = -crop_bott
+    # get sizes of images
+    ts_width, ts_height = [*ts_ims.values()][0].size
+    box_width, box_height = [*box_ims.values()][0].size
 
-        curr_im = add_margin(curr_im.crop(crop_box), *margins_to_add)
+    # set sizes of margins to be cropped and replaced with white
+    crop_left = 0
+    crop_top = 0
+    crop_right = 0
+    crop_bott = 0
 
-        # add some space between
 
-        px, py = j*width, i*height
-        if i > 0:
-            py = py-(i*crop_top)
-        out_im.paste(curr_im,(px,py))
+    # generate the paneled output image
+    nrow=2
+    ncol=2
+    out_im = Image.new('RGB', ts_width*1.1 + box_width*1.1)
+
+
+    # fill the output image with the input images
+    for i, redundancy in ['lo', 'hi']:
+
+                out_im.paste(curr_im,(px,py))
 
 # save image to file
 if plot_type == 'DENS':
